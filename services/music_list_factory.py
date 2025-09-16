cecbe3899573c747487ceafb735c29640ff561c2
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-09-16 11:00:44 -0300

    if dont have lyric search lyric by link

--- services/music_list_factory.py
+++ services/music_list_factory.py
@@ -14,9 +14,13 @@ class MusicListFactory:
         
     def create_music_list(self):
         try:
-            for music in self.musicDTO_list:
-                extractor = LyricExtractorManager(music.link)
-                lyric = extractor.get_lyric()
-                self.music_list.append(Music(music.name, lyric))
+            for musicDTO in self.musicDTO_list:
+                if musicDTO.lyric == "":
+                    extractor = LyricExtractorManager(musicDTO.link)
+                    lyric = extractor.get_lyric()
+                    self.music_list.append(Music(musicDTO.name, lyric))
+                else:
+                    self.music_list.append(Music(musicDTO.name, musicDTO.lyric))
+
         except Exception as e:
             raise RuntimeError(f"Erro ao criar lista músicas: {e}")
