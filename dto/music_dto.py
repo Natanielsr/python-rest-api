11a180ffcf76e3a9a1484f9bbd3d5ca7582b9c33
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-09-16 10:59:54 -0300

    add lyric to dto

--- dto/music_dto.py
+++ dto/music_dto.py
@@ -1,7 +1,8 @@
 class MusicDTO:
-    def __init__(self, name, link):
+    def __init__(self, name, link, lyric):
         self.name = name
         self.link = link
+        self.lyric = lyric
 
     def to_dict(self):
-        return {'name': self.name, 'link': self.link}
+        return {'name': self.name, 'link': self.link, 'lyric': self.lyric}
